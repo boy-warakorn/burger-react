eb7cfc8d08358bd5528ec8419d83115d590223e6
boy <boydoubleb@gmail.com>
2020-05-25 20:55:23 +0700

    [ADD] update responsive navbar&modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Aux from "../../../hoc/Auxilary";
+import Button from "../../UI/Button/Button";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const orderSummary = (props) => {
+  const ingredientsSummary = Object.keys(props.ingredients).map((igKey) => {
+    return (
+      <ListItemText key={igKey}>
+        <span
+          style={{
+            textTransform: "capitalize",
+          }}
+        >
+          {igKey}
+        </span>
+        : {props.ingredients[igKey]}
+      </ListItemText>
+    );
+  });
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      {ingredientsSummary}
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Aux>
+  );
+};
+
+export default orderSummary;
